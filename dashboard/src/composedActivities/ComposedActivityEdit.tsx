21a764427fb0bc679f58af5c429f8e7e2e23e884
Matteo Chini <matteo.chini@gmail.com>
2023-07-31 11:16:41 +0200

    aligned learning scenario Id param

--- dashboard/src/composedActivities/ComposedActivityEdit.tsx
+++ dashboard/src/composedActivities/ComposedActivityEdit.tsx
@@ -22,10 +22,10 @@ const PostEditActions = () => {
   const recordId = useGetRecordId();
   const [searchParams, setSearchParams] = useSearchParams();
   const domainId = searchParams.get(DOMAIN_URL_PARAM);
-  const scenarioId = searchParams.get(SCENARIO_URL_PARAM);
+  const learningScenarioId = searchParams.get(SCENARIO_URL_PARAM);
   const moduleId = searchParams.get(MODULO_URL_PARAM);
   const fragmentId = searchParams.get(FRAGMENT_URL_PARAM);
-  const to = `/composed-activities/${recordId}/show??${DOMAIN_URL_PARAM}=${domainId}&${SCENARIO_URL_PARAM}=${scenarioId}&${MODULO_URL_PARAM}=${moduleId}&${FRAGMENT_URL_PARAM}=${fragmentId}`;
+  const to = `/composed-activities/${recordId}/show??${DOMAIN_URL_PARAM}=${domainId}&${SCENARIO_URL_PARAM}=${learningScenarioId}&${MODULO_URL_PARAM}=${moduleId}&${FRAGMENT_URL_PARAM}=${fragmentId}`;
   if (!recordId) return null;
   return (
     <>
@@ -39,12 +39,12 @@ export const ComposedActivityEdit = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const redirect = useRedirect();
   const domainId = searchParams.get(DOMAIN_URL_PARAM);
-  const scenarioId = searchParams.get(SCENARIO_URL_PARAM);
+  const learningScenarioId = searchParams.get(SCENARIO_URL_PARAM);
   const moduleId = searchParams.get(MODULO_URL_PARAM);
   const fragmentId = searchParams.get(FRAGMENT_URL_PARAM);
   const onSuccess = () => {
     redirect(
-      `/composed-activities??${DOMAIN_URL_PARAM}=${domainId}&${SCENARIO_URL_PARAM}=${scenarioId}&${MODULO_URL_PARAM}=${moduleId}&${FRAGMENT_URL_PARAM}=${fragmentId}`
+      `/composed-activities??${DOMAIN_URL_PARAM}=${domainId}&${SCENARIO_URL_PARAM}=${learningScenarioId}&${MODULO_URL_PARAM}=${moduleId}&${FRAGMENT_URL_PARAM}=${fragmentId}`
     );
   };
   return (
